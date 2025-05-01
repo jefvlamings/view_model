3c60c4f05c3ce14fa39f374223d7550ffd7a77dc
Jef Vlamings <vlamingsjef@gmail.com>
2025-05-01 17:01:10 +0200

    update test comment

--- test/view_model_integration_test.rb
+++ test/view_model_integration_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ViewModelIntegrationTest < ActionDispatch::IntegrationTest
-  test "renders viewmodel output in h1" do
+  test "with a view model, renders attributes" do
     product = products(:one)
     get product_path(product)
     assert_select "h1", product.name
